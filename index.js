e5d1dc6497dd5d9181fbe7af977ee097667177de
Saim Amir <saimxamir@gmail.com>
2023-03-28 04:27:48 -0400

    add function to print pass times

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = (passTimes) => {
+  for (const e of passTimes) {
+
+    //We pass the risetime value as an argument to the Date constructor after multiplying it by 1000. This is because risetime is in Unix timestamp format, which is in seconds since January 1, 1970, whereas Date works with Unix timestamp format in milliseconds.
+
+    const riseTimeDate = new Date(e.risetime * 1000);
+    const printStatement = `Next pass at ${riseTimeDate} for ${e.duration} seconds!`;
+    console.log(printStatement);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
-  // success, print out the deets!
-  console.log(passTimes);
+
+  printPassTimes(passTimes);
 });
-
-
-// for (const key in passes) {
-//   const riseTimeDate = new Date(passes.risetime * 1000);
-//   const passTimes = `Next pass at ${riseTimeDate} for ${passes.duration} seconds`;
-//   callback(null,passTimes);
