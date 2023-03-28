3cf762d097c5f13200a217fb45de5d80be6de2d7
Saim Amir <saimxamir@gmail.com>
2023-03-28 00:27:18 -0400

    add error handling and conditional statements

--- iss.js
+++ iss.js
@@ -16,8 +16,26 @@ const fetchMyIP = function(callback) {
   //use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
     console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body); // Print the IP
+
+    if(error) {
+      //pass through the error to the callback if an error occurs when requesting the IP data.
+      callback(error,null);
+    } else {
+      //parse and extract the IP address using JSON and then pass that through to the callback (as the second argument) if there is no error.
+      const extractedIP = JSON.parse(body);
+      callback(null,extractedIP["ip"]);
+    }
+
+    
+    
+
+
+
+
+
+
   });
 };
 
