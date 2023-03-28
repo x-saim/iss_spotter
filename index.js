c1f235a346ffc09cce9b2bb11be7883a7f6bd6b5
Saim Amir <saimxamir@gmail.com>
2023-03-28 00:19:23 -0400

    add files to repo and boiler plate code items

---
+++ index.js
@@ -0,0 +1,12 @@
+//main fetch function.
+
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
---
+++ iss.js
@@ -0,0 +1,24 @@
+//logic for fetching the data from each API endpoint.
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
+const request = require("request");
+
+//fetchMyIP which will asynchronously return our IP Address using an API.
+const fetchMyIP = function(callback) {
+  //use request to fetch IP address from JSON API
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+};
+
+module.exports = { fetchMyIP };
