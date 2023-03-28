3272154a9240365dfc9397868c4c7482a0a9aa92
Saim Amir <saimxamir@gmail.com>
2023-03-28 04:30:43 -0400

    add function desc

--- iss.js
+++ iss.js
@@ -1,6 +1,15 @@
 const request = require("request");
 
-//fetchMyIP which will asynchronously return our IP Address using an API.
+
+/**
+ * fetchMyIP makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+
 const fetchMyIP = function(callback) {
   //use request to fetch IP address from JSON API
   request('https://api.ipify.org/?format=json', (error, response, body) => {
@@ -20,6 +29,17 @@ const fetchMyIP = function(callback) {
   }
   );
 };
+
+/**
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
+ * Input:
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
+ */
 
 const fetchCoordsByIP = function(ip, callback) {
   request(ip, (error, response, body) => {
@@ -41,6 +61,16 @@ const fetchCoordsByIP = function(ip, callback) {
   );
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
 const fetchISSFlyOverTimes = (coords, callback) => {
   const searchLink = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
@@ -61,7 +91,6 @@ const fetchISSFlyOverTimes = (coords, callback) => {
 
   );
 };
-
 
 const nextISSTimesForMyLocation = (callback) => {
   //fetchMyIP() nested callback Implementation
